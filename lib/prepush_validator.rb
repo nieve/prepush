ada8127414e486d2c7770c5d66221b1a22341fac
Nieve <nievegoor@gmail.com>
2013-05-23 21:40:15 +0100

    added more validation to the renamed prepush_validator

--- lib/prepush_validator.rb
+++ lib/prepush_validator.rb
@@ -1,6 +1,14 @@
 module PrePush
-	class TestRunnerValidator
+	class Validator
 		def self.validate runner
+			if !Dir.exists?('.git')
+				puts "Couldn't find a git repository"
+				return false
+			end
+			if !Dir.exists?('.git/hooks')
+				puts "Couldn't find the git hooks dir"
+				return false
+			end
 			bin = File.dirname(__FILE__)
 			runners_dir = "#{bin}/../lib/runners"
 			found = false
