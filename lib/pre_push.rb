4f328840557ee589091f8c77c98958f83c18c71c
Nieve <nievegoor@gmail.com>
2013-05-24 23:50:42 +0100

    sorted executing test runner & removed tmp tracing

--- lib/pre_push.rb
+++ lib/pre_push.rb
@@ -21,21 +21,12 @@ module PrePush
 			$?.success?
 		end
 	  def run_tests assemblies
-	  	puts '1'
 	  	set_exes_cache
-	  	puts '2'
 			gem_lib = File.dirname(__FILE__)
-	  	puts '3'
 			assemblies = assemblies || [@solution]
-	  	puts assemblies
 			assemblies.each do |assembly|
-				puts assembly
 				exe = @runners_exes[@test_runner]
-				puts exe
-	  		puts "#{gem_lib}/runners/#{@test_runner}/#{exe} #{assembly}"
-	  		system "#{gem_lib}/runners/#{@test_runner}/#{exe} #{assembly}"
-	  		puts 'ran'
-	  		puts $?.success?
+	  		system "\"#{gem_lib}/runners/#{@test_runner}/#{exe}\" \"#{assembly}\""
 	  		$?.success?
 	  	end
 	  end
@@ -47,6 +38,7 @@ module PrePush
 		  	bin = File.dirname(__FILE__)
 				runners_dir = "#{bin}/../lib/runners"
 				Dir.entries(runners_dir).each {|file| @runners_exes[file] = Dir.entries("#{runners_dir}/#{file}").detect{|f| f.end_with?('.exe')}}	
+				@runners_exes['nunit262'] = 'nunit-console.exe'
 			end
 	  end
 	end
