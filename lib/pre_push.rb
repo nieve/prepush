64054d14227719bce085f52e688a492c6193d1ad
Nieve <nievegoor@gmail.com>
2013-05-24 21:52:38 +0100

    renamed nunit-agent.exe

--- lib/pre_push.rb
+++ lib/pre_push.rb
@@ -15,27 +15,43 @@ module PrePush
 			end
 		end
 		def build
+			set_exes_cache
 			msbuild = 'C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe'
 			system "#{msbuild} #{@solution}"
 			$?.success?
 		end
 	  def run_tests assemblies
+	  	puts '1'
+	  	set_exes_cache
+	  	puts '2'
 			gem_lib = File.dirname(__FILE__)
+	  	puts '3'
 			assemblies = assemblies || [@solution]
+	  	puts assemblies
 			assemblies.each do |assembly|
+				puts assembly
 				exe = @runners_exes[@test_runner]
+				puts exe
+	  		puts "#{gem_lib}/runners/#{@test_runner}/#{exe} #{assembly}"
 	  		system "#{gem_lib}/runners/#{@test_runner}/#{exe} #{assembly}"
+	  		puts 'ran'
+	  		puts $?.success?
 	  		$?.success?
 	  	end
+	  end
+
+	  private
+	  def set_exes_cache
+	  	if (@runners_exes == nil || @runners_exes.empty?) then
+				@runners_exes = {}
+		  	bin = File.dirname(__FILE__)
+				runners_dir = "#{bin}/../lib/runners"
+				Dir.entries(runners_dir).each {|file| @runners_exes[file] = Dir.entries("#{runners_dir}/#{file}").detect{|f| f.end_with?('.exe')}}	
+			end
 	  end
 	end
 
 	def self.included(receiver)
-		@runners_exes = {}
-		bin = File.dirname(__FILE__)
-		runners_dir = "#{bin}/../lib/runners"
-		Dir.entries(runners_dir).each {|file| @runners_exes[file] = Dir.entries(file).detect{|f| f.end_with?('.exe')}}
-
 		receiver.extend         ClassMethods
 		#receiver.send :include, InstanceMethods
 	end
